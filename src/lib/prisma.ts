2919d61ee5deb9c31b82a68da3edb635cc5c2151
CLEVERSON  SANTOS <152625169+FELIPE-NOBREGA06@users.noreply.github.com>
2025-08-12 22:58:15 -0400

    Banco de dados
    fazendo integração do bando de dados

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+import { Prisma, PrismaClient } from "@prisma/client"
+
+let prisma: PrismaClient
+
+if(process.env.NODE_ENV === 'production'){
+    prisma = new PrismaClient();
+}else{
+    let gloolWithPrisma = global as typeof globalThis & {
+        prisma: PrismaClient;
+    }
+
+    if(!gloolWithPrisma.prisma){
+        gloolWithPrisma.print = new PrismaClient();
+    }
+
+    prisma = gloolWithPrisma.prisma;
+}
+
+export default prisma;
